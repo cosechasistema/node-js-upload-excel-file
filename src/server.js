5d8dbb03fab36a938735da4d31c9d0d49d708c27
cosechasistema <cosechasistema@gmail.com>
2023-02-09 11:12:26 -0300

    Se agrego manejp pm2

--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
 const express = require("express");
-require("dotenv").config();
+// require("dotenv").config();
 const chalk = require("chalk");
+
+const path = require("path");
+
+// La opciones son development o production, se toma del process.json
+let PM2_ENVIROMENT = process.env.NODE_ENV || "development";
+
+require("dotenv").config({
+  path: path.join(__dirname, PM2_ENVIROMENT + ".env"),
+});
+console.log(chalk.green(`Tipo enviroment:  ${PM2_ENVIROMENT} `));
+
+// require("dotenv").config({
+//   path: "/home/dubchristian/desarrollo/node_excel_api/node-js-upload-excel-file/src/.env",
+// });
+
 // require("dotenv").config({ path: ".env" });
 const app = express();
 const db = require("./models");
